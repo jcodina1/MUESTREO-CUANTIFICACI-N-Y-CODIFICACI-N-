5fd6781ffe022e10d08f0cc0a29580b246fb4bfd
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-09-03 22:50:09 -0500

    Update Punto1.m

--- Punto1.m
+++ Punto1.m
@@ -1,68 +1,89 @@
-  clc
-    clear 
-    
-    %ítem A. Graficar la señal entre 0 y 1ms
-    f0=1000;
-    fmax=5000;
-    t=0:1e-6:1e-3;
-    A=5;
-    X=A*(sin(f0*2*pi*t) +((1/3)*sin(3*f0*2*pi*t)) +((1/5)*sin(5*f0*2*pi*t)));
-    
-    figure(1)
-     plot(t,X,'b','LineWidth',3)
-    grid on
-    
-    ylabel('Amplitud')
-    xlabel('Tiempo(ms)')
-    title('Gráfica de la señal x(t) entre 0 y 1 ms')
-    hold on
-    %ítem B. Muestrear la señal a 2 veces el valor de su frecuencia máxima
-    fs=2*fmax;
-    n=fs;
-    Ts=0:1/fs:1e-3;
-    Xn=A*(sin(f0*2*pi*Ts) +((1/3) *sin(3*f0*2*pi*Ts)) +((1/5)*sin(5*f0*2*pi*Ts)));
-    
-     stem(Ts,Xn,'r','LineWidth',3)
-    grid on
-    
-    ylabel('Amplitud')
-    xlabel('Tiempo(ms)')
-    title('Gráfica de la señal x(t) muestreada 2 veces el valor de su frecuencia máxima')
-    hold off
-    %ítem C. Muestrear la señal a 4 veces el valor de su frecuencia máxima
-    fs=4*fmax;
-    n=fs;
-    Ts=0:1/fs:1e-3;
-    Xn=A*(sin(f0*2*pi*Ts) +((1/3) *sin(3*f0*2*pi*Ts)) +((1/5)*sin(5*f0*2*pi*Ts)));
-    
-    figure(2)
-     plot(t,X,'b','LineWidth',3)
-    grid on
-    ylabel('Amplitud')
-    xlabel('Tiempo(ms)')
-    title('Gráfica de la señal x(t) entre 0 y 1 ms')
-    hold on
-     stem(Ts,Xn,'r','LineWidth',3)
-    grid on
-    ylabel('Amplitud')
-    xlabel('Tiempo(ms)')
-    title('Gráfica de la señal x(t) muestreada 4 veces el valor de su frecuencia máxima')
-    hold off
-    
-    %%ítem D. Cuantificar la señal usando 5 bits.
-    bits =5;
-    xq=round(Xn*((2^(bits))-1))/((2^(bits))-1);
-    xr=spline(Ts,(xq),t);
-    error=abs(Xn-xq);
-    error_max=max(error);
-    figure(3)
-    plot(t,X,'k');
-    hold on
-    stem(Ts,Xn,"filled",'bo')
-    stem(Ts,xq,"filled",'ro')
-    plot(t,xr,'g')
-    ylabel('Amplitud')
-    xlabel('Tiempo(s)')
-    title('Gráfica de la señal X(t) muestreada, codificada y cuantificada a 5 bits')
-    hold off
-    grid on
+ clc
+clear 
+
+%ítem A. Graficar la señal durante 3 periodos
+t=0:0.001:1;
+f=3;
+A=5;
+pha=0;
+ft=A*cos(2*pi*f*t+pha);
+
+figure(1)
+%subplot(2,1,1), plot(t,ft,'b','LineWidth',3)
+grid on
+
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) durante 3 períodos')
+
+%ítem B. Muestreo de la señal a 4 veces el valor de la frecuencia
+fs=4*f;
+n=fs;
+Ts=(0:n)./fs;
+fn=A*cos(2*pi*f*Ts);
+
+%subplot(2,1,2), stem(Ts,fn,'r','LineWidth',3)
+grid on
+
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) muestreada a 4 veces el valor de la frecuencia')
+
+%ítem C. Muestreo de la señal a 16 veces el valor de la frecuencia
+fs=16*f;
+n=fs;
+Ts=(0:n)./fs;
+fn=A*cos(2*pi*f*Ts);
+
+figure(2)
+%subplot(2,1,1), plot(t,ft,'b','LineWidth',3)
+grid on
+
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) durante 3 períodos')
+%subplot(2,1,2), stem(Ts,fn,'r','LineWidth',3)
+grid on
+
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) muestreada a 16 veces el valor de la frecuencia')
+
+%ítem D. Muestreo de la señal a 1.3 veces el valor de la frecuencia
+
+fs=1.3*f;
+n=fs;
+Ts=(0:n)./fs;
+fn=A*cos(2*pi*f*Ts);
+
+figure(3)
+%subplot(2,1,1), plot(t,ft,'b','LineWidth',3)
+grid on
+
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) durante 3 períodos')
+%subplot(2,1,2), stem(Ts,fn,'r','LineWidth',3)
+grid on
+
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) muestreada a 1.3 veces el valor de la frecuencia')
+
+%ítem E. Cuantificar y codificar la señal usando 3 bits.
+bits =3;
+fq=(fn*((2^(bits))-1))/((2^(bits))-1);
+fr=spline(Ts,(fq),t);
+error=abs(fn-fq);
+error_max=max(error);
+figure(4)
+%plot(t,ft,'k');
+hold on
+stem(Ts,fn,"filled",'bo')
+stem(Ts,fq,"filled",'g')
+plot(t,fr,'bo')
+ylabel('Amplitud')
+xlabel('Tiempo(s)')
+title('Gráfica de la señal f(t) muestreada, codificada y cuantificada a 3 bits')
+hold off
+grid on
